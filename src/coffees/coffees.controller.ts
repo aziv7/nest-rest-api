45642e93abd6ad2e85ebc45ba310edbb8fd4e43d
Aziz Jerbi <57321183+aziv7@users.noreply.github.com>
2023-12-16 21:08:04 +0100

    route with param

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('/all')
   findAll() {
     return [];
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return 'this is coffee ' + id;
   }
 }
